85146eb647865e23c2bd461bc7c4cbc15cae175f
Shivam Shekhar <45821075+shiv-shekhar@users.noreply.github.com>
2020-10-01 23:09:38 +0530

    Update mergesort.py

--- divide_and_conquer/mergesort.py
+++ divide_and_conquer/mergesort.py
@@ -1,10 +1,10 @@
-def merge(arr, left, mid, right):
+def merge(arr, low, mid, high):
     # overall array will divided into 2 array
     # left_arr contains the left portion of array from left to mid
     # right_arr contains the right portion of array from mid + 1 to right
-    left_arr = arr[left : mid + 1]
-    right_arr = arr[mid + 1 : right + 1]
-    k = left
+    left_arr = arr[low : mid + 1]
+    right_arr = arr[mid + 1 : high + 1]
+    k = low
     i = 0
     j = 0
     while i < len(left_arr) and j < len(right_arr):
@@ -27,21 +27,21 @@ def merge(arr, left, mid, right):
     return arr
 
 
-def mergesort(arr, left, right):
+def mergesort(arr, low, high):
     """
     >>> mergesort([3, 2, 1], 0, 2)
     [1, 2, 3]
     >>> mergesort([3, 2, 1, 0, 1, 2, 3, 5, 4], 0, 8)
     [0, 1, 1, 2, 2, 3, 3, 4, 5]
     """
-    if left < right:
-        mid = (left + right) // 2
+    if low < high:
+        mid = (low + high) // 2
         # print("ms1",a,b,m)
-        mergesort(arr, left, mid)
+        mergesort(arr, low, mid)
         # print("ms2",a,m+1,e)
-        mergesort(arr, mid + 1, right)
+        mergesort(arr, mid + 1, high)
         # print("m",a,b,m,e)
-        merge(arr, left, mid, right)
+        merge(arr, low, mid, high)
         return arr
 
 
